5c6b3c7307a998d3788183b72a61e7dd862f419d
Iulia Batinas <iulia.batinas@accesa.eu>
2023-10-19 18:00:45 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Batinas_Iulia_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
