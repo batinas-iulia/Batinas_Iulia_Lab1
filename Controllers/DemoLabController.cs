dea7b113f50730e036bf841d557216544d081c85
Iulia Batinas <iulia.batinas@accesa.eu>
2023-10-19 18:42:16 +0300

    exercises for the first lab done

---
+++ Controllers/DemoLabController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Batinas_Iulia_Lab1.Controllers
+{
+    public class DemoLabController : Controller
+    {
+        //
+        // GET: /welcome/
+        public string Index()
+        {
+            return "Welcome!";
+        }
+
+        //
+        // GET: /greetingsOfTheUser/
+        public string greetingsOfTheUser()
+        {
+            return "Greetings, user!";
+        }
+
+
+     
+        private string anotherGreeting(string u, int nr)
+        {
+            return u + ", you are here for the " + nr.ToString() + "st time!";
+        }
+
+        public string callAnotherGreeting() 
+        {
+            return anotherGreeting("User", 1);
+        }
+    }
+}
